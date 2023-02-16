0a23c57b684ab7a46cf01a4a092720c8c7fc7371
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2023-02-16 09:57:55 +0100

    Create productList amd Product components and data.js

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/14774750/pexels-photo-14774750.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      link: "https://images.pexels.com/photos/14774750/pexels-photo-14774750.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      img: "",
+      link: "https://images.pexels.com/photos/14774750/pexels-photo-14774750.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
